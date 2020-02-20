2db0f664c46da32f2ee2a701746d86141a08be1f
Christine Dang <dangc101@gmail.com>
2020-02-20 03:25:43 -0500

    html & JS starting point

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+var DogBreeds = ["Labrador", "Golden Retriever", "German Shepard", "Doberman", "Chihuahua", "Bishon Frise", "Shih Tzu", "Pomeranian"];
+    console.log(DogBreeds);
+
+
+//word needs to be chosen at random at start of game
+//use ... DogBreeds.length, (math.floor(math.random())
+var ChosenWord = DogBreeds[Math.floor(Math.random()*DogBreeds.length)]; 
+    console.log(ChosenWord);
+
+
+var html = "<p>Dog Breed: " + ChosenWord + "</p>";
+document.querySelector('#footer').innerHTML = html;
+
